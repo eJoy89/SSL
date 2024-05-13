8a5341b035a27c7f4a261bde987902b97b178db6
unknown <cho89@live.co.kr>
2024-05-13 15:26:06 +0900

    attribute

--- src/components/CameraControl.vue
+++ src/components/CameraControl.vue
@@ -6,7 +6,7 @@
   </template>
   
   <script>
-  import { ref } from 'vue'; 
+import { ref, onMounted } from 'vue'; 
   
   export default {
     name: "CameraControl",
@@ -23,10 +23,34 @@
             })
             .catch(function(error) {
               console.log("카메라 접근에 실패했습니다:", error);
-            });
+            }); 
         } else {
           console.error("브라우저가 카메라 기능을 지원하지 않습니다.");
         }
+
+        onMounted(() => {
+          const script = document.createElement('script');
+          script.src = "https://code.jquery.com/jquery-3.7.1.min.js";
+          script.integrity = "sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=";
+          script.crossOrigin = "anonymous";
+          document.head.appendChild(script);
+
+          script.onload = () => console.log('jQuery loaded successfully');
+          script.onerror = () => console.error('Error loading jQuery');
+        });
+
+        // const loadScript = () => {
+        //   const skScript = document.createElement("script");
+        //   skScript.src = "https://pay-auth.sk-pay.co.kr/pages/js/v3/lib/crypto/encryptedUserAgent.js"
+        //   // skScript.setAttribute("src", "https://pay-auth.sk-pay.co.kr/pages/js/v3/lib/crypto/encryptedUserAgent.js");
+        //   document.head.appendChild(skScript);  
+        //   console.log(skScript)
+
+        // }
+
+        // onMounted(() => {
+        //   loadScript();
+        // });
       };
   
       return {
